b03a86b3ab9f64db77bacc7855b42959cccf9e57
smail859 <75447456+smail859@users.noreply.github.com>
2025-04-21 16:10:23 +0200

    fix puppeteer-core only

--- .render-build.sh
+++ .render-build.sh
@@ -3,8 +3,5 @@
 echo "📦 Installation des dépendances..."
 npm install
 
-echo "🌐 Installation de Chrome via Puppeteer..."
-npx puppeteer browsers install chrome
-
 echo "🚀 Lancement du serveur Node..."
 npm run server
