042e54596b157e3fc63f8b9bac34cdb301619b6a
smail859 <elhajjarsmail70000@gmail.com>
2025-04-22 14:26:42 +0200

    Ajout trace version FED6FEF

--- serveur/server.js
+++ serveur/server.js
@@ -82,6 +82,8 @@ updateDates();
 const startServer = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ VERSION FED6FEF EN LIGNE ✅");
+
 
     // Création des collections si elles n’existent pas
     await UpdateLog.createCollection();
