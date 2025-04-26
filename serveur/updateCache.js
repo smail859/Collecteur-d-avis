6a764ce74f46ca8529e95d97c799ed80fc083c62
smail859 <75447456+smail859@users.noreply.github.com>
2025-04-26 15:49:44 +0200

    Correction cache

--- serveur/updateCache.js
+++ serveur/updateCache.js
@@ -1,6 +1,3 @@
-// -------------------------
-// Mise à jour du cache
-// -----------------------
 const { Review } = require("./model/model");
 const cache = require("memory-cache");
 
@@ -15,18 +12,24 @@ const updateCache = async () => {
 
   const updatedReviews = await Review.find();
 
-  const groupedReviews = updatedReviews.reduce((acc, review) => {
-    if (!acc[review.site]) {
-      acc[review.site] = {
+  const groupedGoogleReviews = {};
+  const groupedTrustpilotReviews = {};
+
+  for (const review of updatedReviews) {
+    const target = review.source === "trustpilot" ? groupedTrustpilotReviews : groupedGoogleReviews;
+    
+    if (!target[review.site]) {
+      target[review.site] = {
         data_id: review.data_id,
         reviews: [],
       };
     }
-    acc[review.site].reviews.push(review);
-    return acc;
-  }, {});
+    target[review.site].reviews.push(review);
+  }
 
-  cache.put("reviews", groupedReviews, cacheDuration);
+  cache.put("reviews", groupedGoogleReviews, cacheDuration);
+  cache.put("trustpilot_reviews", groupedTrustpilotReviews, cacheDuration);
+
   console.log("Cache mis à jour.");
   console.log("Nombre total d’avis en base :", updatedReviews.length);
 };
